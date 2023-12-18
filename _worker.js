366301e49cd5530dfbbd709fafd4590be661d6cc
GaskueNya7 <153889498+GaskueNya7@users.noreply.github.com>
2023-12-19 06:18:04 +0700

    Update indoo_worker.js

--- _worker.js
+++ _worker.js
@@ -6,7 +6,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'b737d9b3-bc34-446c-86fd-31b9a8fc8481';
 
-const proxyIPs = ['163.181.19.169'];
+const proxyIPs = ['163.181.19.152'];
 
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
