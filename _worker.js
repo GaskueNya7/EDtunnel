35e9b88d563efab8b0a9f4009c580c5e7ca55ede
GaskueNya7 <153889498+GaskueNya7@users.noreply.github.com>
2023-12-19 13:43:42 +0700

    Update INDOVANET _worker.js

--- _worker.js
+++ _worker.js
@@ -6,7 +6,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = '44665571-e170-4c9c-95eb-850baf3c880e';
 
-const proxyIPs = ['210.186.12.244'];
+const proxyIPs = ['202.158.139.117'];
 
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
