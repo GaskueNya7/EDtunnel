9cf1808bffd2d6510739eb1ba53e6f0a012e95f8
GaskueNya7 <153889498+GaskueNya7@users.noreply.github.com>
2023-12-24 22:49:12 +0700

    Update linode_worker.js

--- _worker.js
+++ _worker.js
@@ -6,7 +6,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = '44665571-e170-4c9c-95eb-850baf3c880e';
 
-const proxyIPs = ['103.19.58.57'];
+const proxyIPs = ['2600:3c0c::f03c:94ff:fe16:1e3f'];
 
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
