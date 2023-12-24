489d6f84f1c354e1f85afa459cc40c251a5103aa
GaskueNya7 <153889498+GaskueNya7@users.noreply.github.com>
2023-12-24 19:45:16 +0700

    Update amazon id _worker.js

--- _worker.js
+++ _worker.js
@@ -6,7 +6,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = '44665571-e170-4c9c-95eb-850baf3c880e';
 
-const proxyIPs = ['149.129.242.127'];
+const proxyIPs = ['108.137.90.229'];
 
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
